cbe4d542e9eb4112a02d2ad7b8cb14aa77d1d74c
Zero|Bot <admin@zerobot.ru>
2020-08-31 13:40:03 +0300

    Коммит 3108201339
    Добавлен пример работы с бинарными файлами(версия 2)

--- file/file-binary.py
+++ file/file-binary.py
@@ -2,18 +2,37 @@
 import pickle
 
 # Константа с именем файла
-FILNAME = 'test.dat'
+FILENAME = 'test.dat'
 
 name = 'User'
 age = 1
 
 # Записывваем несколько строк в файл
-with open(FILNAME, 'wb') as file:
+with open(FILENAME, 'wb') as file:
+    # С помощью dump происходит последовательная запись объъектов
     pickle.dump(name, file)
     pickle.dump(age, file)
 
 # Читаем данные из файла
-with open(FILNAME, 'rb') as file:
+with open(FILENAME, 'rb') as file:
+    # Поэтому при чтении используем load, для последовательного чтения объектов
     name = pickle.load(file)
     age = pickle.load(file)
     print('Name:', name, '\nAge:', age)
+
+# Версия с записью и чтением набора объектов
+users = [
+    ["User1", 1, True],
+    ["User2", 2, False],
+    ["User3", 3, False]
+]
+
+# Записывваем объект в файл
+with open(FILENAME, "wb") as file:
+    pickle.dump(users, file)
+
+# Читаем объект из файла
+with open(FILENAME, "rb") as file:
+    users_from_file = pickle.load(file)
+    for user in users_from_file:
+        print("Name:", user[0], "\nAge:", user[1], "\nPython developer:", user[2])
